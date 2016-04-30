8761a224299a94aca3edb176525c38682b0b3358
Johannes Unterstein <unterstein@me.com>
2016-04-30 22:02:05 +0200

    refactored build scripts

---
+++ sources/ElasticSearch/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+docker build -t elasticsearch:latest .
+
--- sources/build.sh
+++ sources/build.sh
@@ -2,21 +2,8 @@
 
 cd $(dirname $0)
 
-cd ArticleService
-mvn clean install
-docker build -t articleservice:latest .
-cd ..
-
-cd BasketService
-mvn clean install
-docker build -t basketservice:latest .
-cd ..
-
-cd CheckoutService
-mvn clean install
-docker build -t checkoutservice:latest .
-cd ..
-
-cd WebService
-docker build -t webservice:latest .
-cd ..
+./ArticleService/build.sh
+./BasketService/build.sh
+./CheckoutService/build.sh
+./WebService/build.sh
+./ElasticSearch/build.sh
