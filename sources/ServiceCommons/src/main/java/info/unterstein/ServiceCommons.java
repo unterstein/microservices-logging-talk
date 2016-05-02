967973ef4f8a2f13283f7a65b5de7fc4477a9591
Johannes Unterstein <unterstein@me.com>
2016-05-02 23:58:07 +0200

    logging

--- sources/ServiceCommons/src/main/java/info/unterstein/ServiceCommons.java
+++ sources/ServiceCommons/src/main/java/info/unterstein/ServiceCommons.java
@@ -1,13 +1,18 @@
 package info.unterstein;
 
 import org.jooby.Request;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
 public class ServiceCommons {
 
+  private static final Logger log = LoggerFactory.getLogger(ServiceCommons.class);
+
   public static void beforeRequest(Request req) {
-    MDC.put("requestId", ("" + req.header("X-Request-Id")).replace("[", "").replace("]",""));
-    MDC.put("sessionId", ("" + req.header("X-Session-Id")).replace("[", "").replace("]",""));
+    MDC.put("requestId", ("" + req.header("X-Request-Id")).replace("[", "").replace("]", ""));
+    MDC.put("sessionId", ("" + req.header("X-Session-Id")).replace("[", "").replace("]", ""));
+    log.info("Service called for request " + req);
   }
 
   public static void afterRequest(Request req) {
