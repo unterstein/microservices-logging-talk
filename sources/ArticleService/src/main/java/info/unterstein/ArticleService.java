681a68d3756e0345226df5b853b52fd13018d355
Johannes Unterstein <unterstein@me.com>
2016-05-02 14:33:58 +0200

    jooby logs to elasticsearch as well

--- sources/ArticleService/src/main/java/info/unterstein/ArticleService.java
+++ sources/ArticleService/src/main/java/info/unterstein/ArticleService.java
@@ -1,14 +1,24 @@
 package info.unterstein;
 
 import org.jooby.Jooby;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 /**
  * @author jooby generator
  */
 public class ArticleService extends Jooby {
 
+  private static final Logger log = LoggerFactory.getLogger(ArticleService.class);
+
   {
-    get("/", () -> "Hi articles!");
+    get("/", (req, rsp) -> {
+      MDC.put("requestId", "" + req.header("X-Request-Id"));
+      log.info("Hi articles!");
+      rsp.send("Hi articles!");
+      MDC.clear();
+    });
   }
 
   public static void main(final String[] args) throws Exception {
