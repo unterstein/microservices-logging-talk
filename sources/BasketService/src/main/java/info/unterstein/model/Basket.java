19ac72049fad74ece1b7fe40d2921dabd70bc00b
Johannes Unterstein <unterstein@me.com>
2016-05-03 22:04:52 +0200

    better basket implementation

--- sources/BasketService/src/main/java/info/unterstein/model/Basket.java
+++ sources/BasketService/src/main/java/info/unterstein/model/Basket.java
@@ -1,19 +1,32 @@
 package info.unterstein.model;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Johannes Unterstein (unterstein@me.com)
  */
 public class Basket {
-  private Map<Long, BasketArticle> articles = new HashMap<>();
+  private List<BasketArticle> articles = new ArrayList<>();
 
   public void addArticle(BasketArticle article) {
-    articles.put(article.id, article);
+    for (BasketArticle basketArticle : articles) {
+      if (basketArticle.id.equals(article.id)) {
+        basketArticle.quantity += article.quantity;
+        return;
+      }
+    }
+    articles.add(article);
   }
 
   public void removeArticle(Long id) {
-    articles.remove(id);
+    BasketArticle articleToRemove = null;
+    for (BasketArticle article : articles) {
+      if (article.id.equals(id)) {
+        articleToRemove = article;
+        break;
+      }
+    }
+    articles.remove(articleToRemove);
   }
 }
