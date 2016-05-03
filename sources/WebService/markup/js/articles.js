7ac9d520af0ab6e6436f58b3619f7207b5314194
Johannes Unterstein <unterstein@me.com>
2016-05-03 21:54:44 +0200

    add stuff to basket

---
+++ sources/WebService/markup/js/articles.js
@@ -0,0 +1,25 @@
+$(function() {
+  $.get("/articles/", function(data) {
+    var viewModel = ko.mapping.fromJS(data);
+    ko.applyBindings(viewModel);
+
+    $(".add-article").click(function(e) {
+      var that = this;
+      var article = {
+        id: $(that).data("id"),
+        name: $(that).data("name"),
+        price: $(that).data("price"),
+        quantity: 1
+      };
+      $.ajax({
+        url: "/basket/",
+        type: 'PUT',    
+        data: JSON.stringify(article),
+        contentType: 'json',
+        success: function(result) {
+            alert("success?");
+        }
+      });
+    });
+  });
+});
